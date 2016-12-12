457815c68b83813b269d1aeccb6da2842134126d
Diogo Lundberg <diogo@lundberg.com.br>
2016-12-12 17:26:22 -0200

    Adiciona estorias do dojo no README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# dojo-secex
+# Dojo
+
+## Estórias
+
+Eu enquanto analista gostaria de:
+
+ - visualizar o valor total importado por produto.
+
+ - visualizar o valor total por cidade
+
+ - visualilzar o total de produtos por porto, que tenha pelo menos 5 produtos diferentes 
+
+
+## Requisitos
+
+Para obter melhor perfomance, preciso que o formato do JSON não repita a estrutura.
+Portanto seu formato será
+
+``` javascript
+    {
+        "columns" : [<colunas>],
+        "data" : [<dados>]
+    }
+```
--- config.py
+++ config.py
@@ -1,27 +1,6 @@
 import os
 
 
-class ProjectPath(str):
-
-    def __init__(self):
-        self._project_path = os.getcwd()
-
-    def __add__(self, path):
-        self._project_path = os.path.join(self._project_path, path)
-        return self
-
-    def __radd__(self, text):
-        self._project_path = text + self._project_path
-        return self
-
-    def __str__(self):
-        return self._project_path
-    
-    def __repr__(self):
-        return self._project_path
-
-project_path = ProjectPath()
-
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -34,7 +13,7 @@ class Production(Config):
 
 class Development(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite://' + project_path + 'develop.db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://secex'
 
 
 class Testing(Config):
