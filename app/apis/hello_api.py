fb34af4ec4fa99aba6815c4fea83ef8b327317c9
gilmardealcantara <gilmardealcantara@gmail.com>
2016-12-16 11:00:46 -0200

    Add visualization and update requirements.

--- app/apis/hello_api.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-blueprint = Blueprint('hello_api', __name__, url_prefix='/api/hello')
-
-
-@blueprint.route('/')
-def index():
-    return jsonify(hello='world'), 200
